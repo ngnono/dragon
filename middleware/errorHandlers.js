5d2a7ac2b5a8b319036ca2e0487e7cb0cc1a1296
liangyali <me@liangyali.com>
2014-12-17 14:38:07 +0800

    add my page

--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var _ = require('lodash');
-var errors =require('errorHandlers');
+var errors =require('./errorHandlers');
 
 exports.fileNotFound = function (template) {
     return function (req, res, next) {

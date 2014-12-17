f9c0cf5a54a60fa8d43cdfcf08340807eb2478df
liangyali <me@liangyali.com>
2014-12-17 13:21:13 +0800

    add my page

--- app/controllers/wechat/api/index.js
+++ app/controllers/wechat/api/index.js
@@ -1,22 +1,21 @@
 "use strict";
 
-module.exports=function(router){
-    module.exports = function (router) {
 
-        router.use('/', wechat.process());
+module.exports = function (router) {
 
-        /**
-         * 文本消息处理
-         */
-        wechat.on('text', function (session) {
-            session.replyTextMessage("Received:http://liangyali.duapp.com/my" + session.incomingMessage.Content);
-        });
+    router.use('/', wechat.process());
 
-        /**
-         * 用户订阅公共账号处理消息
-         */
-        wechat.on('event.subscribe', function (session) {
-            session.replyTextMessage("欢迎关注！");
-        });
-    };
+    /**
+     * 文本消息处理
+     */
+    wechat.on('text', function (session) {
+        session.replyTextMessage("Received:http://liangyali.duapp.com/my" + session.incomingMessage.Content);
+    });
+
+    /**
+     * 用户订阅公共账号处理消息
+     */
+    wechat.on('event.subscribe', function (session) {
+        session.replyTextMessage("欢迎关注！");
+    });
 };
